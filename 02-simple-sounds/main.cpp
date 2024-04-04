8e8501e6e64a0fcb67b31bc27dfdb9afe838fc69
Robert Albus <hello@robertalbus.com>
2024-04-03 20:51:33 -0600

    Use InterfaceV2 in 02-simple-sounds

--- 02-simple-sounds/main.cpp
+++ 02-simple-sounds/main.cpp
@@ -1,25 +1,74 @@
+#include "Clover/Algorithm/Envelope/Basic.h"
 #include "Clover/Algorithm/Wavetable/Generators.h"
 #include "Clover/Algorithm/Wavetable/WavetableOscillatorMono.h"
-#include "Clover/IO/Interface.h"
+#include "Clover/IO/InterfaceV2.h"
 #include "Clover/Util/MusicTime.h"
 
+#include <cstdio>
+
 constexpr int SAMPLE_RATE = 48000;
+constexpr int DURATION = 2 * SAMPLE_RATE;
 
 using namespace Clover;
+
 int main(int argc, char *argv[])
 {
+
+    float initialFreq = 1000;
     Wavetable::Wavetable<float> sineTable = Wavetable::Sine<float>(512);
     Wavetable::WavetableOscillatorMono<float> osc(SAMPLE_RATE, sineTable);
+    osc.freq(initialFreq);
+    Envelope::Basic<float> envelope(0.f, initialFreq - 30.f, (uint)DURATION);
 
-    IO::Interface interface;
+    IO::InterfaceV2 interface;
+    IO::SystemAudioIoConfig systemAudioConfig = IO::GetSystemAudioIoConfig();
+    // systemAudioConfig.printDevices();
+
+    try {
+        interface.openDevice({
+            -1,
+            0,
+            6,
+            2,
+            SAMPLE_RATE,
+            0
+        });
+    } catch(IO::Exception e) {
+        printf("%s\n", e.what());
+        return 1;
+    }
+
     Util::Time time(160, Clover::Base::sampleRate, &interface.clock);
 
+    interface.setAudioCallback([&](IO::AudioCallbackArguments data) {
+        float &L = *(data.output);
+        float &R = *(data.output + 1);
 
+        float signal = osc.process() * 0.5;
+        float envelopeValue = envelope.process();
+        osc.freq(initialFreq - envelopeValue);
+        L = signal;
+        R = signal;
 
+        printf("%f - %f - %f\n", *(data.output), *(data.output + 1), signal);
+        printf("%p - %p\n", (void*)data.output, (void*)(data.output + 1));
 
+        printf("%i\n", data.currentClockSample);
+        if (data.currentClockSample == DURATION) {
+            return IO::AudioCallbackStatus::END;
+        }
+        return IO::AudioCallbackStatus::CONTINUE;
+    });
 
+    try {
+        interface.start();
+    } catch (IO::Exception e) {
+        printf("%s\n", e.what());
+        return 1;
+    }
 
-
+    interface.waitForStreamComplete();
+    interface.stop();
 
     return 0;
 }
